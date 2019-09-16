ae9d8566022607f8cd99ba88fa69031043277565
Yoeran Luteijn <github@yoeran.nl>
2019-09-16 16:08:32 +0200

    Update multiple-displays plugin
    Changing the API to work with Electron v6

--- plugins/multiple-displays.plugin.js
+++ plugins/multiple-displays.plugin.js
@@ -1,40 +1,49 @@
-const LOCALSTORAGE_KEY = 'plugin:selectedScreen'
+const LOCALSTORAGE_KEY = 'plugin:selectedScreen';
 
 function setScreen(displayIdx, displays) {
-    const chosenDisplay = displays[displayIdx]
-    localStorage.setItem(LOCALSTORAGE_KEY, chosenDisplay.id)
-    return chosenDisplay
+    const chosenDisplay = displays[displayIdx];
+    localStorage.setItem(LOCALSTORAGE_KEY, chosenDisplay.id);
+    return chosenDisplay;
 }
 
 module.exports = function MultipleDisplaysPlugin(api) {
-    const setBounds = (display) => {
-        api.electron.remote.getCurrentWindow().setBounds(display.bounds)
+    const setBounds = display =>
+        api.electron.remote.getCurrentWindow().setBounds(display.bounds);
+
+    const displays = api.electron.remote.screen.getAllDisplays();
+
+    if (displays.length <= 1) {
+        return;
     }
 
-    const displays = api.electron.screen.getAllDisplays()
-    if (displays.length <= 1) { return }
-
     api.electron.remote.globalShortcut.register('CmdOrCtrl+Shift+L', () => {
-        localStorage.removeItem(LOCALSTORAGE_KEY)
-        api.electron.remote.app.quit()
-    })
+        localStorage.removeItem(LOCALSTORAGE_KEY);
+        api.electron.remote.app.quit();
+    });
 
-    const selectedScreen = localStorage.getItem(LOCALSTORAGE_KEY) || ''
-    const display = displays.find(d => d.id.toString() === selectedScreen.toString())
+    const selectedScreen = localStorage.getItem(LOCALSTORAGE_KEY) || '';
+    const display = displays.find(
+        d => d.id.toString() === selectedScreen.toString()
+    );
 
-    if (display) { return setBounds(display) }
+    if (display) {
+        return setBounds(display);
+    }
 
-    localStorage.removeItem(LOCALSTORAGE_KEY)
-    api.electron.remote.dialog.showMessageBox({
-        type: 'question',
-        buttons: displays.map(d => `${d.size.width}x${d.size.height}`),
-        defaultId: 0,
-        title: 'Please select a display',
-        message: 'Please select a display',
-        detail: `${displays.length} displays found, but none selected. Select one based on it's dimensions.`,
-        noLink: true,
-    }, (response) => {
-        const chosenDisplay = setScreen(response, displays)
-        setBounds(chosenDisplay)
-    })
-}
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    api.electron.remote.dialog.showMessageBox(
+        {
+            type: 'question',
+            buttons: displays.map(d => `${d.size.width}x${d.size.height}`),
+            defaultId: 0,
+            title: 'Please select a display',
+            message: 'Please select a display',
+            detail: `${displays.length} displays found, but none selected. Select one based on it's dimensions.`,
+            noLink: true,
+        },
+        response => {
+            const chosenDisplay = setScreen(response, displays);
+            setBounds(chosenDisplay);
+        }
+    );
+};
